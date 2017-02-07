72fe0414633de863d76cfb1807a8251e5879fbc3
PRAKHAR YADAV <prakhar.821@gmail.com>
2017-02-07 23:36:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ button.onClick = function() {
     
     
     
-    counter=counter + 1;
+    var counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
     
