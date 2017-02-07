8759d9abd4086bc30f8518389cc3ec109001ce23
PRAKHAR YADAV <prakhar.821@gmail.com>
2017-02-07 23:25:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.get('/article-three', function(req, res) {
   res.send('Article three requested');    
 });
 
+var counter = 0;
+
+app.get('/counter' , function(req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
