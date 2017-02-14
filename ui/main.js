eb8801a58bca17ef4b262f0e16b6a483efbe8f4f
PRAKHAR YADAV <prakhar.821@gmail.com>
2017-02-14 23:26:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,15 +35,15 @@ submit.onclick = function() {
                 list += '<li>' + name[i] + '<li>' ;
         
              }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
+         document.getElementById('namelist').innerHTML = list;
+        //ul.innerHTML = list;
         }
        }
    };
    
     var nameInput = document.getElementById('name');
     var name = nameInput.value; 
-    request.open('GET', 'http://prakhar-821.imad.hasura-app.io/submit-name?name='+name, true);
+    request.open('GET', 'http://prakhar-821.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
    
